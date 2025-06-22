f28c70b92ab9603c289ff97bbb83b78d28b0133a
Zuhair <khanzuhair96@live.co.uk>
2025-06-22 14:05:30 +0100

    tests for account class

--- lib/account.py
+++ lib/account.py
@@ -1,3 +1,6 @@
+class InsufficientFundsError(Exception): # this is a custom exception - new concept for me - googled it - user defined exception errors - so instead of using the ValueError - "Defining a custom exception, therefore, gives you more granularity. It allows you to proceed in different ways depending on which exact type of error happened in your application." 
+    pass
+
 class Account:
     def __init__(self, deposit):
         if deposit <= 0:
--- tests/test_account.py
+++ tests/test_account.py
@@ -8,3 +8,11 @@ def test_account_requires_initial_deposit():
 def test_account_initializes_with_balance():
     account = Account(100)
     assert account.balance == 100
+
+def test_deposit_increases_balance():
+
+def test_withdraw_decreases_balance():
+
+def test_withdraw_insufficient_funds():
+
+def test_transaction_history_tracking():
